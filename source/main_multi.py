ce16e0350932f49f657944a9685ab87e235a35c1
Luca Morreale <luca.morreale93@gmail.com>
2020-03-25 16:48:08 +0000

    Minor changes
    Change path of input object.
    Change camera location.
    Set number of views and rotation axis as global variables.

--- source/main_multi.py
+++ source/main_multi.py
@@ -1,25 +1,26 @@
 
 import os
-import bpy
-import mathutils
 import sys
-import numpy as np
 
 sys.path.append('.')
 from blender import Blender
 
 
+up_axis = 'Y'
+num_view = 10
+
 wrapper = Blender()
 wrapper.set_engine('BLENDER_WORKBENCH')
-# wrapper.set_image_size(1900, 1080)
-wrapper.set_light_location((1,1,1))     # TODO what is a good position for the light?
-# wrapper.set_cam_location((0, 0, -0.35))
-wrapper.set_cam_location((0, 0, -5.5))
-# wrapper.fix_camera_yaw(-3.14)
-wrapper.fix_camera_yaw(0)
+wrapper.set_light_location((1,1,1))
+wrapper.set_cam_location((0, -0.05, -2.5))
+wrapper.fix_camera_yaw(-3.14)
 wrapper.point_camera_to_origin()
 
-path = '/home/luca/projects/phd/LearnDecoder_prj/remote_sabine/workspace/LearnDecoder/visualization/reconstruction_faust_flat_ae_arch-2048-2048-4-2048-2048_wlinear_batch-1_loss-chamfer_opt-sgd_LR-0.1_l2-0.0_reglamda-0.0_1582118407.983123/train/'
+wrapper.set_view_solid()
+wrapper.set_color_by_vertex()
+
+path = '../models/b3.obj'
+
 
 if os.path.isfile(path):
     files_list = [path]
@@ -31,15 +32,13 @@ for file in files_list:
 
     if file[-3:] != 'ply' and file[-3:] != 'obj':
         continue
-    if 'mesh' not in file:
-        continue
 
     wrapper.load_object(file)
 
     fila_name = file.split('/')[-1]
     fila_name = fila_name[:fila_name.rfind('.')]
 
-    wrapper.render_views_rotating(fila_name, num_view=10, up_axis='Y')
+    wrapper.render_views_rotating(fila_name, num_view=num_view, up_axis=up_axis)
 
     wrapper.remove_objects()
 
